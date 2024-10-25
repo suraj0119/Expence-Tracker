7f6af7934c757e62523fe7589d6a3f622766fcf0
suraj0119 <surajkamble1319@gmail.com>
2024-10-25 19:20:50 +0530

    Authentication using clerk

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Dashboard } from './pages/Dashboard/Dashboard'
+import Auth from './pages/Auth/Auth'
 function App() {
   return (
     <>
       <Router>
         <div className="app-container">
           <Routes>
-              <Route path='/' element={<h1>Dashboard</h1>} />
-              <Route path='/auth' element={<h1>SignIn</h1>} />
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/auth' element={<Auth />} />
           </Routes>
         </div>
       </Router>
